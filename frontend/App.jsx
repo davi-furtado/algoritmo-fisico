9eaa9e6b4a752e8880eed0727eae6ac21bfb772b
Davi Reis Furtado <vitor25davi30@gmail.com>
2026-05-21 07:31:47 -0300

    Atualizar o App.jsx

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -17,7 +17,7 @@ import InsertPhotoBtn from './components/InsertPhotoBtn'
 import CodeBox from './components/CodeBox'
 import SegmentedToggle from './components/SegmentedToggle'
 
-const url = 'https://algoritmo-fisico.vercel.app/'
+const url = 'http://SEU_IP_AQUI:8000/'
 
 export default function App() {
   const [image, setImage] = useState(null)
@@ -93,7 +93,10 @@ export default function App() {
       const res = await fetch(url, {
         method: 'POST',
         body: form,
-        headers: { Accept: 'application/json' }
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          Accept: 'application/json'
+        }
       })
       if (res.status === 200) {
         const data = await res.json()
